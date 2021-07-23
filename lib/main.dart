139e40916c0a04ae25dfc4d83392ea48db1348b2
loren <lorencisneros00@gmail.com>
2021-07-23 21:40:18 +0800

    Saving data in FIREBASE after registration.

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,38 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:tradeoff5/screens/home.dart';
+import 'package:tradeoff5/screens/login_screen.dart';
 import 'package:tradeoff5/screens/main.dart';
 import 'package:tradeoff5/screens/registration_screen.dart';
 import 'package:tradeoff5/screens/welcome_screen.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:tradeoff5/services/helper.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MultiProvider(
+      providers: [
+        Provider<AuthService>(
+            create: (_) => AuthService(FirebaseAuth.instance)),
+        StreamProvider(
+          initialData: () {},
+          create: (context) => context.read<AuthService>().authStateChanges,
+        )
+      ],
+      child: App(),
+    );
+  }
+}
+
+class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -20,6 +44,7 @@ class MyApp extends StatelessWidget {
         RegistrationScreen.id: (context) => RegistrationScreen(),
         Home.id: (context) => Home(),
         Main.id: (context) => Main(),
+        LoginScreen.id: (context) => LoginScreen(),
       },
     );
   }
